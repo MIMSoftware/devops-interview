cecba036a3bf905f7a53bd3d3766d42ac446d4bc
Jonathan Papcun <jpapcun@mimsoftware.com>
2022-05-02 12:27:20 -0400

    Updated versions to remove vulnerabilities and resolved bintray related build errors.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,11 @@ val logback_version: String by project
 
 plugins {
     application
-    kotlin("jvm") version "1.4.32"
+    kotlin("jvm") version "1.6.21"
 }
 
 group = "com.mimsoftware"
-version = "0.0.1"
+version = "0.0.2"
 
 application {
     mainClassName = "io.ktor.server.netty.EngineMain"
@@ -19,8 +19,6 @@ application {
 
 repositories {
     mavenCentral()
-    maven { url = uri("https://kotlin.bintray.com/ktor") }
-    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven") }
 }
 
 dependencies {
@@ -30,6 +28,7 @@ dependencies {
     implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-html-builder:$ktor_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
 }
 
 kotlin.sourceSets["main"].kotlin.srcDirs("src")
